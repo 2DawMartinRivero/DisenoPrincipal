107af1f7e97cc4f02118798aa12c977dd5fe7536
BienveClase <bienve@clase>
2023-03-21 12:42:27 +0100

    Añadido ruta a AccesoServidor2daw

---
+++ target/generated-sources/annotations/com/daw/principalservidor2daw/DTO/Proyectos_.java
@@ -0,0 +1,20 @@
+package com.daw.principalservidor2daw.DTO;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-21T12:41:36")
+@StaticMetamodel(Proyectos.class)
+public class Proyectos_ { 
+
+    public static volatile SingularAttribute<Proyectos, String> descripcion;
+    public static volatile SingularAttribute<Proyectos, Date> fecha;
+    public static volatile SingularAttribute<Proyectos, String> ruta;
+    public static volatile SingularAttribute<Proyectos, Integer> codProyecto;
+    public static volatile SingularAttribute<Proyectos, String> logo;
+    public static volatile SingularAttribute<Proyectos, String> nombre;
+    public static volatile SingularAttribute<Proyectos, String> participantes;
+
+}
