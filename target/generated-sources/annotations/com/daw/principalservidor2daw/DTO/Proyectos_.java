518787a4b363339df061265866704930d8a43a3b
BienveClase <bienve@clase>
2023-03-23 13:29:19 +0100

    Versión final

--- target/generated-sources/annotations/com/daw/principalservidor2daw/DTO/Proyectos_.java
+++ target/generated-sources/annotations/com/daw/principalservidor2daw/DTO/Proyectos_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-21T12:41:36")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-03-23T13:24:58")
 @StaticMetamodel(Proyectos.class)
 public class Proyectos_ { 
 
